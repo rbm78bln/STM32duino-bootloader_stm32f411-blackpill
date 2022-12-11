125264dbdfeaab6a8fbce389a85bbc98b524c0eb
Ralf <43565949+rbm78bln@users.noreply.github.com>
2022-12-11 03:03:47 +0100

    v1.0.0

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,11 @@
 test -f Makefile || cd ..
 
 make clean
-# make DFU_USER_CONFIG=userconfig.h stm32f103x8 # bluepill
-# make DFU_USER_CONFIG=userconfig.h stm32f103xb # stink baite
-# make DFU_USER_CONFIG=userconfig.h stm32f401xe
-make DFU_USER_CONFIG=userconfig.h stm32f411xe # blackpill
+# make DFU_USER_CONFIG=userconfig.h stm32f103x8 	# bluepill
+# make DFU_USER_CONFIG=userconfig.h stm32f103xb 	# stink baite
+# make DFU_USER_CONFIG=userconfig.h stm32f401xe 	# blackpill
+# make DFU_USER_CONFIG=userconfig.h stm32f401xe_0x2000 	# blackpill
+# make DFU_USER_CONFIG=userconfig.h stm32f411xe 	# blackpill
+# make DFU_USER_CONFIG=userconfig.h stm32f411xe_0x2000 	# blackpill
 
 ls -l build/
